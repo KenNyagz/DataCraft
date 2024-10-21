bea155af8495d84af3a75a5ca1a7180121ef8520
Ken Nyaga <112327222+KenNyagz@users.noreply.github.com>
2024-10-22 00:30:08 +0300

    Update settings.py

--- freelancerCove/freelancerCove/settings.py
+++ freelancerCove/freelancerCove/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'freelance_cove',
         'USER': 'root',
-        'PASSWORD': '@Kennyaga',
+        'PASSWORD': '@K',
         'HOST': 'localhost',
         'PORT': '3306'
     }
